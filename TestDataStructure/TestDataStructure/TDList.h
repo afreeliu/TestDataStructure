e23625634d3649851ae10637e7e1177c9af1cbed
lufee_new_mini <afree_liu@163.com>
2019-04-16 07:48:47 +0800

    bug

--- TestDataStructure/TestDataStructure/TDList.h
+++ TestDataStructure/TestDataStructure/TDList.h
@@ -4,7 +4,7 @@
 //
 //  Created by TB on 2019/4/15.
 //  Copyright © 2019 wo. All rights reserved.
-//
+//  单链表
 
 #import <Foundation/Foundation.h>
 
@@ -13,6 +13,19 @@ NS_ASSUME_NONNULL_BEGIN
 @interface TDList : NSObject
 /// 初始化一个固定大小的线形表
 - (instancetype)initWithSize:(NSUInteger)size;
+
+/// 查找链表中的某个元素
+- (BOOL)checkListIsHaveItem:(id)item;
+
+/// 插入一个元素到链表中
+- (BOOL)insertItem:(id)item;
+
+/// 删除链表中的一个元素
+- (BOOL)deleteItem:(id)item;
+
+/// 删除整个链表
+- (BOOL)dropList;
+
 
 
 @end
