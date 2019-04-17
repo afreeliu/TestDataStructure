421fa78756c6c37ea55b8aa8cdab0236f2e471fb
afreeliu <afree_liu@163.com>
2019-04-17 20:28:45 +0800

    bug

---
+++ TestDataStructure/TestDataStructure/TDStaticList.h
@@ -0,0 +1,21 @@
+//
+//  TDStaticList.h
+//  TestDataStructure
+//
+//  Created by TB on 2019/4/16.
+//  Copyright © 2019 wo. All rights reserved.
+//  静态链表 (比较少用，主要是解决一些没有指针的预言实现的单链表的能力的方法）
+
+#import <Foundation/Foundation.h>
+
+
+@interface TDStaticList : NSObject
+
+/// 初始化静态链表
+
+/// 插入元素操作
+
+/// 删除元素操作
+
+
+@end
---
+++ TestDataStructure/TestDataStructure/TDStaticList.m
@@ -0,0 +1,13 @@
+//
+//  TDStaticList.m
+//  TestDataStructure
+//
+//  Created by TB on 2019/4/16.
+//  Copyright © 2019 wo. All rights reserved.
+//
+
+#import "TDStaticList.h"
+
+@implementation TDStaticList
+
+@end
